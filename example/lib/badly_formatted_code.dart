baa1b5158e0f205f52fef6cb480043692237773f
idNoRD <idNoRD@users.noreply.github.com>
2025-07-24 20:20:42 -0700

    Remove garbage from example

--- example/lib/badly_formatted_code.dart
+++ example/lib/badly_formatted_code.dart
@@ -1,10 +1,6 @@
-import 'package:flutter/foundation.dart';
-
 /// Example of badly formatted code
 /// Uncomment:
 ///void main(){print('hello world');}
 void main() {
-  if (kDebugMode) {
-    print('hello world2');
-  }
+  print('hello world');
 }
